adb1a041f0218d1bb44b8706c9a634cd4b19d95b
Zeru15 <zeruf5@gmail.com>
2026-06-03 13:33:10 +0300

    modified index

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 "use strict";
-var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
-var polyfill_1 = require("@js-temporal/polyfill");
-var student = {
+const polyfill_1 = require("@js-temporal/polyfill");
+const student = {
     id: "STU-001",
     name: "Hana Tadesse",
     enrollmentDate: polyfill_1.Temporal.Now.instant(),
 };
 // student.id = "STU-999";
 // console.log(student.gpa.toFixed(2));
-console.log((_b = (_a = student.gpa) === null || _a === void 0 ? void 0 : _a.toFixed(2)) !== null && _b !== void 0 ? _b : "Not Yet Graded");
+console.log(student.gpa?.toFixed(2) ?? "Not Yet Graded");
